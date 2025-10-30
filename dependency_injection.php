b922587139990b97e99c6bcab024bcf58ee1e7f4
LMouad <lechqermouad@gmail.com>
2025-10-30 11:03:26 +0100

    Update dependency_injection.php add another methode

--- dependency_injection.php
+++ dependency_injection.php
@@ -11,6 +11,15 @@ class Start implements Engine{
         echo $this->brand." engine start";
     }
 }
+class Stop implements Engine{
+    private $brand;
+    public function __construct($brand){
+        $this->brand=$brand;
+    }
+    public function engineState(){
+        echo $this->brand." engine stop";
+    }
+}
 class Car{
     private $eng;
     public function __construct(Engine $eng){
@@ -23,5 +32,8 @@ class Car{
 $eng = new Start('toyota');
 $car = new Car($eng);
 $car->drive();
+$eng1 = new Stop('toyota');
+$car1 = new Car($eng1);
+$car1->drive();
 
 ?>
